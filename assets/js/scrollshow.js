6beb81b9f345835c814d81b5a8a7c463edb39e97
lakserk <lakserk@yahoo.com>
2020-05-12 00:09:36 +1000

    12may

---
+++ assets/js/scrollshow.js
@@ -0,0 +1,33 @@
+$(document).scroll(function () {
+    /*Show element after user scrolls 800px
+    var y = $(this).scrollTop();
+    if (y > 800) {
+        $('.bottomMenu').fadeIn();
+    } else {
+        $('.bottomMenu').fadeOut();
+    }*/
+
+    // Show element after user scrolls past 
+    // the top edge of its parent 
+    var y = $(this).scrollTop();
+    $('#belobara').each(function () {
+        var t = $('#deiktis1').offset().top;
+        if (y > t) {
+            $(this).fadeIn();
+        } else {
+            $(this).fadeOut();
+        }
+    });
+});
+
+
+
+$(document).ready(function(){
+
+    $("#buttondown").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#blogposts").offset().top
+        }, 1000);
+    });
+  
+  }); 
