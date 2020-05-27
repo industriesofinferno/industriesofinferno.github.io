f97111fefe28d2bb930aa8d2ce9f227cbfdfa91b
lakserk <lakserk@yahoo.com>
2020-05-27 23:27:35 +1000

    sada

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     extend: {
       height: theme => ({
+        "screen/15": "80vh",
         "screen/2": "50vh",
         "screen/3": "calc(100vh / 3)",
         "screen/4": "calc(100vh / 4)",
